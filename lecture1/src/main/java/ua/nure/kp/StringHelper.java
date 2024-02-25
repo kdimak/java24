210c9f0b4c9f61864c9644e7b5b63a1376e60816
Dmitriy Kinoshenko <dkinoshenko@gmail.com>
2024-02-25 22:17:02 +0100

    Lecture 1

---
+++ lecture1/src/main/java/ua/nure/kp/StringHelper.java
@@ -0,0 +1,11 @@
+package ua.nure.kp;
+
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack,
+// or -1 if needle is not part of haystack.
+public class StringHelper {
+    
+    public int findsFirstOccurrence(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+    
+}
